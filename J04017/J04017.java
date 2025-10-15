1f98ea41fabc5cb680c001791d2edd9c07ec52d2
k4nnguyen <grilledog296@gmail.com>
2025-10-15 12:03:23 +0700

    Two more class subject

---
+++ J04017/J04017.java
@@ -0,0 +1,76 @@
+import java.util.*;
+class Matrix{
+    private int n,m;
+    private long[][] a;
+    public Matrix(int n, int m)
+    {
+        a = new long[n][m];
+        this.n = n;
+        this.m = m;
+    }
+    public void nextMatrix(Scanner scanner){
+        for(int i = 0; i<n; i++)
+        {
+            for(int j = 0; j<m; j++)
+            {
+                a[i][j] = scanner.nextInt();
+            }
+        }
+    }
+    public Matrix mul(Matrix b)
+    {
+        Matrix res = new Matrix(n, b.m);
+
+        for(int i = 0; i<n; i++)
+        {
+            for(int j = 0; j<b.m; j++)
+            {
+                long tmp = 0;
+                for(int k = 0; k<m; k++)
+                {
+                    tmp += a[i][k] * b.a[k][j];
+                }
+                res.a[i][j] = tmp;
+            }
+        }
+        return res;
+    }
+    public Matrix trans(){
+        Matrix b = new Matrix(m,n);
+        for(int i = 0; i<n; i++)
+        {
+            for(int j =0; j<m; j++)
+            {
+                b.a[j][i] = a[i][j];
+            }
+        }
+        return b;
+    }
+    @Override
+    public String toString(){
+        String[] s = new String[n];
+        for(int i = 0; i<n; i++)
+        {
+            s[i] = "";
+            for(int j = 0; j<m; j++)
+            {
+                s[i] += a[i][j] + " ";
+            }
+        }
+        return String.join("\n", s);
+    }
+}
+
+public class J04017 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t-->0){
+             int n = sc.nextInt(), m = sc.nextInt();
+             Matrix a = new Matrix(n,m);
+             a.nextMatrix(sc);
+             Matrix b = a.trans();
+             System.out.println(a.mul(b));
+        }
+    }
+}
